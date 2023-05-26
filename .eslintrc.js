872d9c4c4fc5dae995fdb0b4a2fd199e868caf10
Neket54 <kvartv112@gmail.com>
2023-05-26 14:18:12 +0700

    Refactor i18n plugin rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,11 +41,25 @@ module.exports = {
         'import/extensions': 'off',
         'no-underscore-dangle': 'off',
         'import/no-extraneous-dependencies': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': [
+            'error',
+            {
+                markupOnly: true,
+                ignoreAttribute: ['data-testid'],
+            },
+        ],
         'max-len': ['error', { ignoreComments: true }],
         'no-restricted-globals': 'off',
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
