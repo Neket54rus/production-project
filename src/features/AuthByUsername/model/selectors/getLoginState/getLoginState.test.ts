e7d85edb7e7a589085eea8ae5de37e5cf6ea5258
Neket54 <kvartv112@gmail.com>
2023-06-13 15:47:07 +0700

    Add tests for login module

---
+++ src/features/AuthByUsername/model/selectors/getLoginState/getLoginState.test.ts
@@ -0,0 +1,22 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginState } from './getLoginState';
+
+describe('getLoginState', () => {
+	test('should return login state', () => {
+		const state: DeepPartial<StateSchema> = {
+			loginForm: {
+				username: 'admin',
+				password: '123',
+				isLoading: false,
+			},
+		};
+
+		expect(getLoginState(state as StateSchema)).toEqual({
+			username: 'admin',
+			password: '123',
+			isLoading: false,
+		});
+	});
+});
