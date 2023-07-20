ab2c98b26305c6ed8e356ec31c3f07fbc942cb60
Neket54rus <nikitaevodkimovit@gmail.com>
2023-07-20 13:41:29 +0700

    Fix drawer, add browser list config

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -9,7 +9,6 @@ import { Drawer } from '@/shared/ui/Drawer/Drawer';
 import { Icon } from '@/shared/ui/Icon/Icon';
 import { Popover } from '@/shared/ui/Popups';
 
-import { AnimationProvider } from '@/shared/lib/components/AnimationProvider';
 import cls from './NotificationButton.module.scss';
 
 interface NotificationButtonProps {
@@ -50,11 +49,9 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
 			</BrowserView>
 			<MobileView>
 				{trigger}
-				<AnimationProvider>
-					<Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-						<NotificationList />
-					</Drawer>
-				</AnimationProvider>
+				<Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+					<NotificationList />
+				</Drawer>
 			</MobileView>
 		</div>
 	);
--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, memo, useCallback, useEffect } from 'react';
 
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { useAnimationLibs } from '@/shared/lib/components/AnimationProvider';
+import { AnimationProvider, useAnimationLibs } from '@/shared/lib/components/AnimationProvider';
 
 import { Overlay } from '../Overlay/Overlay';
 import { Portal } from '../Portal';
@@ -101,7 +101,7 @@ export const DrawerContent = memo((props: DrawerProps) => {
 	);
 });
 
-export const Drawer = memo((props: DrawerProps) => {
+const DrawerAsync = (props: DrawerProps) => {
 	const { isLoaded } = useAnimationLibs();
 
 	if (!isLoaded) {
@@ -109,4 +109,10 @@ export const Drawer = memo((props: DrawerProps) => {
 	}
 
 	return <DrawerContent {...props} />;
-});
+};
+
+export const Drawer = (props: DrawerProps) => (
+	<AnimationProvider>
+		<DrawerAsync {...props} />
+	</AnimationProvider>
+);
